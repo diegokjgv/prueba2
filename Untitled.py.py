23ac9493cbe67d4f0c5ca32fed656d8252d7f33f
diegokjgv <134401520+diegokjgv@users.noreply.github.com>
2023-05-23 10:00:38 -0400

    Add files via upload

---
+++ Untitled.py.py
@@ -0,0 +1,31 @@
+cotizacion=0
+tratamiento=0
+total=0
+trabajador=0
+print("Bienvenido a la clinica dental el diente de oro")
+print("Seleccione tratamiento")
+print("Los tratamientos disponibles son Carillas porcelana , Implantes dentales , Ortodoncia Brackets")
+print("Que tratamiento quiere")
+if tratamiento=1 
+print("El valor es de 250.000")
+cotizacion+250.000
+else tratamiento=2
+print("el valor es de 475.000")
+    cotizacion+475.000
+elif
+tratamiento=3
+print("el valor es de 800.00")
+    cotizacion+800.000
+print("que trabajador es?")
+if trabajador=1
+    cotizacion/%15
+else
+    trabajador=2
+    cotizacion/%10
+elif
+    trabajador=3
+    cotizacion/%5
+cotizacion/12=total
+print(f{total})
+
+
